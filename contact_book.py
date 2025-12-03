699aafd44e145abb68b9c001dc6daf4f57a459cc
mnv-godhani <godhanik87@gmail.com>
2025-12-03 13:47:37 +0530

    Add, Delete, Exit Added

--- contact_book.py
+++ contact_book.py
@@ -1,4 +1,6 @@
-print("Hello from Contact-Book/contact_book.py")
+# Contact Book Application - mnv_godhani 😉
+
+import time
 
 Contact = [{"name" : "Manav", "phone" : "1234567890", "Email" : "manavgodhnai88@gmail.com"}]
 
@@ -13,11 +15,50 @@ while True:
     choice = int(input("Enter your choice :- "))
 
     if choice == 1:
+        if len(Contact) == 0:
+            print("No Contacts Available! ❌")
+        else:
+            print("----------------------------------------------------------")
+            print    ("No  Name    |    Phone       |     Email  ")
+            print("----------------------------------------------------------")
+            num = 1
+            for data in Contact:
+                print(num, " ",data["name"], "  |  ", data["phone"], "  |  ", data["Email"])
+                num += 1
+            print("----------------------------------------------------------")
+    elif choice == 2:
+        name = input("Enter Name :- ")
+        phone = input("Enter Phone Number :- ")
+        if len(phone) != 10 or not phone.isdigit():
+            print("Invalid Phone Number! ❌")
+            continue
+        email = input("Enter Email :- ")
+        if "@" not in email or "." not in email:
+            print("Invalid Email Address! ❌")
+            continue
+        Contact.append({"name" : name, "phone" : phone, "Email" : email})
         print("----------------------------------------------------------")
-        print    ("Name    |    Phone       |     Email  ")
-        print("----------------------------------------------------------")
-        for data in Contact:
-            print(data["name"], "  |  ", data["phone"], "  |  ", data["Email"])
-
+        print("Contact Added Successfully! ✅")
+    elif choice == 3:
+        if len(Contact) < 1:
+            print("No Contacts Available! for delete ❌")
+            continue
+        else:
+            print("----------------------------------------------------------")
+            print    ("No  Name    |    Phone       |     Email  ")
+            print("----------------------------------------------------------")
+            num = 1
+            for data in Contact:
+                print(num, " ",data["name"], "  |  ", data["phone"], "  |  ", data["Email"])
+                num += 1
+            print("----------------------------------------------------------")
+        delete = int(input("Enter Contact Number to Delete :- "))
+        Contact.pop(delete - 1)
+        print("Contact Deleted Successfully! ✅")
+    elif choice == 4:
+        print("Exiting Contact Book... 👋")
+        time.sleep(3)
+        print("🙏 Sitaram 🙏")
+        break
     else:
         print("Invalid Choice! ❌")
