8d831dc5683baebb7c7d374f85de23d912d74b80
mnv-godhani <godhanik87@gmail.com>
2025-12-03 13:18:40 +0530

    View Contact Done

--- contact_book.py
+++ contact_book.py
@@ -1,1 +1,23 @@
 print("Hello from Contact-Book/contact_book.py")
+
+Contact = [{"name" : "Manav", "phone" : "1234567890", "Email" : "manavgodhnai88@gmail.com"}]
+
+while True:
+    print("\nContact Book Menu:\n")
+    print(" 1️⃣  View Contacts")
+    print(" 2️⃣  Add Contact")
+    print(" 3️⃣  Delete Contact")
+    print(" 4️⃣  Exit\n")
+
+    print("-----------------------------")
+    choice = int(input("Enter your choice :- "))
+
+    if choice == 1:
+        print("----------------------------------------------------------")
+        print    ("Name    |    Phone       |     Email  ")
+        print("----------------------------------------------------------")
+        for data in Contact:
+            print(data["name"], "  |  ", data["phone"], "  |  ", data["Email"])
+
+    else:
+        print("Invalid Choice! ❌")
